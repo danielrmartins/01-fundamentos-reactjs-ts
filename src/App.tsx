4acb868e0cae6b4224cf64adb7195b7cecc79695
Daniel <danielmartins10@gmail.com>
2024-09-03 16:46:47 -0300

    chore: change interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/Header";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -44,12 +44,7 @@ export function App() {
       <Sidebar />
       <main>
         {posts.map(post => (
-          <Post
-            key={post.id}
-            author={post.author}
-            content={post.content}
-            publishedAt={post.publishedAt}
-          />
+          <Post key={post.id} post={post} />
         ))}
       </main>
     </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,24 +18,29 @@ interface Content {
   content: string;
 }
 
-interface PostProps {
+export interface PostType {
+  id: number;
   author: Author;
   publishedAt: Date;
   content: Content[];
 }
 
+interface PostProps {
+  post: PostType;
+}
 
-export function Post({ author, publishedAt, content }: PostProps) {
+
+export function Post({ post }: PostProps) {
   const [comments, setComments] = useState<string[]>([]);
   const [newCommentText, setNewCommentText] = useState('');
 
   const isNewCommentEmpty = newCommentText.length === 0;
 
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+  const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
 
-  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+  const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
     locale: ptBR,
     addSuffix: true
   });
@@ -65,18 +70,18 @@ export function Post({ author, publishedAt, content }: PostProps) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={author.avatarUrl} alt={author.name} />
+          <Avatar src={post.author.avatarUrl} alt={post.author.name} />
           <div className={styles.authorInfo}>
-            <strong>{author.name}</strong>
-            <span>{author.role}</span>
+            <strong>{post.author.name}</strong>
+            <span>{post.author.role}</span>
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+        <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        {content.map((line, index) => {
+        {post.content.map((line, index) => {
           if (line.type === 'paragraph') {
             return <p key={index}>{line.content}</p>;
           } else if (line.type === 'link') {
